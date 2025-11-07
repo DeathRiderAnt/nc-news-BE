81e1a9707811642ad38e0cf01a09063f0dadddeb
DeathRiderAnt <ant_odin@hotmail.co.uk>
2025-11-07 16:17:58 +0000

    updated connections

--- db/connection.js
+++ db/connection.js
@@ -10,13 +10,12 @@ if (ENV === "production") {
   if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL not set in environment variables!");
   }
+  config.connectionString = process.env.DATABASE_URL
+  config.max = 2
 
-  config = { connectionString: process.env.DATABASE_URL, max: 2 };
   console.log("Connecting to production database");
 } else {
   if (!process.env.PGDATABASE) throw new Error("PGDATABASE not set");
-
-  config = { database: process.env.PGDATABASE };
 
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
